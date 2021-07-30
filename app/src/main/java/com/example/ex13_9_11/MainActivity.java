b193455c5c47d2408aa92e9321a4a19aaea59930
최지우 <w2040@e-mirim.hs.kr>
2021-07-30 10:44:17 +0900

    Edit activity_main.xml commit2

--- app/src/main/java/com/example/ex13_9_11/MainActivity.java
+++ app/src/main/java/com/example/ex13_9_11/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.ex13_9_11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar seek1, seek2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Thread 기본");
+        seek1 = findViewById(R.id.seek1);
+        seek2 = findViewById(R.id.seek2);
+        Button btnstart = findViewById(R.id.btn_start);
+
+        btnstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread thread = new Thread() {
+                    @Override
+                    public void run() {
+                        for (int i = seek1.getProgress(); i < 100; i = i + 2){
+                            seek1.setProgress(seek1.getProgress() + 2);
+                        SystemClock.sleep(100);
+                        }
+                    }
+                };
+
+                thread.start();
+
+                Thread thread2 = new Thread() {
+                    @Override
+                    public void run() {
+                        for (int i = seek2.getProgress(); i < 100; i = i + 1){
+                            seek2.setProgress(seek2.getProgress() + 2);
+                            SystemClock.sleep(100);
+                        }
+
+                    }
+                };
+
+                thread2.start();
+            }
+        });
     }
 }
